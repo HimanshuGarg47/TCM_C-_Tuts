efcf1dab3164b1248a5b505a429a23da8b22a20b
Himanshu Garg <78200614+HimanshuGarg47@users.noreply.github.com>
2022-04-20 14:25:13 +0000

    add comment

--- .ccls-cache/@home@runner@TCMCTuts/main.cpp
+++ .ccls-cache/@home@runner@TCMCTuts/main.cpp
@@ -1,9 +1,23 @@
+// first program
 #include <iostream>
 using namespace std;
-int main() {
-  for(int i = 0;i<10;i++)
-    {
-      cout << i << endl;
-    }
-  cout << "Hello World!\n";
+
+// is the single line comment
+
+// this is my main function
+int main()
+{
+
+  // std is the namespace
+  // std::cout << "Hello World";  // print Hello world
+  std::cout << "Hello World";
+  
+  }
+
+
+// used for cout He
+int func(void)
+{
+  int a = 7;
+  cout << a;
 }
